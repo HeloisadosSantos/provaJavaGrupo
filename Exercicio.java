0a3ebaf6c6cb2d2d08c0677a1096a2c96fd3cae4
HeloisadosSantos <heloisadossantos010@gmail.com>
2023-03-29 21:26:29 -0300

    date

--- Exercicio.java
+++ Exercicio.java
@@ -70,40 +70,40 @@ public class Exercicio {
     double imc = peso/(Altura*Altura);
    // I
       if (imc<20 && sexo==1 && idade>5 && idade>16 && idade<25) {
-      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades diciceis como: Luta.  Ganhe entre " + (62.1* Altura - 44.7) + " Kg para estar no seu peso ideal" );
+      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades diciceis como: Luta.  Ganhe entre " + ((62.1* Altura - 44.7) - peso) + " Kg para estar no seu peso ideal" );
    // I  
       }if (imc<20 && sexo==2 && idade>5 && idade>16 && idade<25) {
-      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta.  Ganhe entre " + (72.7* Altura - 58) + " Kg para estar no seu peso ideal");
+      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta.  Ganhe entre " + ((72.7* Altura - 58) - peso) + " Kg para estar no seu peso ideal");
    // II
       }if (imc<20 && sexo==2 && idade>25 && idade<35) {
-      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta,Musculação Intensa.  Ganhe entre " + (72.7* Altura - 58) + " Kg para estar no seu peso ideal");
+      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta,Musculação Intensa.  Ganhe entre " + ((72.7* Altura - 58) - peso) + " Kg para estar no seu peso ideal");
    // II
       }if (imc<20 && sexo==1 && idade>25 && idade<35) {
-      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta,Musculação Intensa.  Ganhe entre " + (62.1* Altura - 44.7) + " Kg para estar no seu peso ideal");
+      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta,Musculação Intensa.  Ganhe entre " + ((62.1* Altura - 44.7) - peso) + " Kg para estar no seu peso ideal");
    // III
       }if (imc<20 && sexo==2 && idade>35 && idade<45) {
-      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta,Musculação Intensa.  Ganhe entre " + (72.7* Altura - 58) + " Kg para estar no seu peso ideal");
+      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta,Musculação Intensa.  Ganhe entre " +  ((72.7* Altura - 58) - peso) + " Kg para estar no seu peso ideal");
    // III
       }if (imc<20 && sexo==1 && idade>35 && idade<45) {
-      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta,Musculação Intensa.  Ganhe entre " + (62.1* Altura - 44.7) + " Kg para estar no seu peso ideal");
+      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta,Musculação Intensa.  Ganhe entre " + ((62.1* Altura - 44.7) - peso) + " Kg para estar no seu peso ideal");
    // IV
       }if (imc<20 && sexo==2 && idade>45 && idade<55) {
-      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta.  Ganhe entre " + (72.7* Altura - 58) + " Kg para estar no seu peso ideal");
+      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta.  Ganhe entre " + ((72.7* Altura - 58) - peso) + " Kg para estar no seu peso ideal");
    // IV
       }if (imc<20 && sexo==1 && idade>45 && idade<55) {
-      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta,Pilates.  Ganhe entre " + (62.1* Altura - 44.7) + " Kg para estar no seu peso ideal");
+      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Luta,Pilates.  Ganhe entre " + ((62.1* Altura - 44.7) - peso) + " Kg para estar no seu peso ideal");
    // V
       }if (imc<20 && sexo==2 && idade>55 && idade<65) {
-      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis.  Ganhe entre " + (72.7* Altura - 58) + " Kg para estar no seu peso ideal");
+      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis.  Ganhe entre " + ((72.7* Altura - 58) - peso) + " Kg para estar no seu peso ideal");
    // V
       }if (imc<20 && sexo==1 && idade>55 && idade<65) {
-      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como:Pilates.  Ganhe entre " + (62.1* Altura - 44.7) + " Kg para estar no seu peso ideal");
+      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como:Pilates.  Ganhe entre " + ((62.1* Altura - 44.7) - peso) + " Kg para estar no seu peso ideal");
    // VI
       }if (imc<20 && sexo==2 && idade>66) {
-      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Pilates.  Ganhe entre " + (72.7* Altura - 58) + " Kg para estar no seu peso ideal");
+      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Pilates.  Ganhe entre " + ((72.7* Altura - 58) - peso) + " Kg para estar no seu peso ideal");
    // VI
       }if (imc<20 && sexo==1 && idade>66) {
-      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Pilates.  Ganhe entre " + (62.1* Altura - 44.7) + " Kg para estar no seu peso ideal");
+      System.out.println(" Seu imc é igual a " + peso/(Altura*Altura) + " está abaixo do peso e deve praticar atividades dificeis como: Pilates.  Ganhe entre " + ((62.1* Altura - 44.7) - peso) + " Kg para estar no seu peso ideal");
 
 
    // I
@@ -147,40 +147,40 @@ public class Exercicio {
 
    // I
       }if (imc>27 && sexo==1 &&  idade>5 && idade>16 && idade<25) {
-      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga " + (62.1* Altura - 44.7) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
+      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga " + ( peso - (62.1* Altura - 44.7)) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
    // I   
       }if (imc>27 && sexo==2 && idade>5 && idade>16 && idade<25) {
-      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga " + (72.7* Altura - 58) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
+      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga " + (peso - (72.7* Altura - 58)) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
    // II   
       }if (imc>27 && sexo==1 && idade>25 && idade<35) {
-      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Yoga " + (62.1* Altura - 44.7) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
+      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Yoga " + (peso - (62.1* Altura - 44.7)) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
    // II  
       }if (imc>27 && sexo==2 && idade>25 && idade<35) {
-      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Yoga " + (72.7* Altura - 58) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
+      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Yoga " + (peso - (72.7* Altura - 58)) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
    // III
       }if (imc>27 && sexo==1 && idade>35 && idade<45) {
-      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Yoga " + (62.1* Altura - 44.7) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
+      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Yoga " + (peso - (62.1* Altura - 44.7)) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
    // III 
       }if (imc>27 && sexo==2 && idade>35 && idade<45) {
-      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Yoga "  + (72.7* Altura - 58) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
+      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Yoga " + (peso - (72.7* Altura - 58)) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
    // IV
       }if (imc>27 && sexo==1 && idade>45 && idade<55) {
-      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação leve,Yoga " + (62.1* Altura - 44.7) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
+      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação leve,Yoga " + (peso - (62.1* Altura - 44.7)) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
    // IV
       }if (imc>27 && sexo==2 && idade>45 && idade<55) {
-      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga " + (72.7* Altura - 58) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );   
+      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga " + (peso - (72.7* Altura - 58)) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );   
    // V 
       }if (imc>27 && sexo==1 && idade>55 && idade<65) {
-      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga " + (62.1* Altura - 44.7) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
+      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga " + (peso - (62.1* Altura - 44.7)) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
    // V
       }if (imc>27 && sexo==2 && idade>55 && idade<65) {
-      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga " + (72.7* Altura - 58) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
+      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga " + (peso - (72.7* Altura - 58)) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
    // VI
       }if (imc>27 && sexo==1 && idade>66 ) {
-      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga " + (62.1* Altura - 44.7) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
+      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga " + (peso - (62.1* Altura - 44.7)) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
    // VI
       }if (imc>27 && sexo==2 && idade>66) {
-      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga  " + (72.7* Altura - 58) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
+      System.out.println( " Seu imc está igual " + peso/(Altura*Altura) + " prátique atividades Leves como: Musculação Leve,Yoga  " + (peso - (72.7* Altura - 58)) + " Kg este é o valor que você precisaria perder para estar no peso ideal." );
    }
       }
    }  
